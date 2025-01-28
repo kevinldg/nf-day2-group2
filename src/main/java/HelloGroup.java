ca9ad83f158b87a4a371e799ebd630a5243e74a5
Yuliia Skrypnyk <jillyjoolli@gmail.com>
2025-01-28 16:54:20 +0100

    add printCalendarForMonth

--- src/main/java/HelloGroup.java
+++ src/main/java/HelloGroup.java
@@ -2,12 +2,21 @@ public class HelloGroup {
     public static void main(String[] args){
         System.out.println("Hi, Everyone. Have fun with codes :D");
 
-        add(1, 2);
+        printCalendarForMonth("January");
+        printCalendarForMonth("February");
+        printCalendarForMonth("March");
+        printCalendarForMonth("April");
+        printCalendarForMonth("May");
+        printCalendarForMonth("June");
+        printCalendarForMonth("July");
+        printCalendarForMonth("August");
+        printCalendarForMonth("September");
+        printCalendarForMonth("October");
+        printCalendarForMonth("November");
+        printCalendarForMonth("December");
     }
 
-    public static int add(int a, int b) {
-        int sum = a + b;
-        System.out.println(sum);
-        return sum;
+    public static void printCalendarForMonth(String monthName) {
+        System.out.println(monthName);
     }
 }
