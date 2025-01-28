1f03a2a910346cd8b64d142cf2a095e143b77f2b
Yuliia Skrypnyk <jillyjoolli@gmail.com>
2025-01-28 16:19:47 +0100

    add important method

--- src/main/java/HelloGroup.java
+++ src/main/java/HelloGroup.java
@@ -1,5 +1,13 @@
 public class HelloGroup {
     public static void main(String[] args){
         System.out.println("Hi, Everyone. Have fun with codes :D");
+
+        add(1, 2);
+    }
+
+    public static int add(int a, int b) {
+        int sum = a + b;
+        System.out.println(sum);
+        return sum;
     }
 }
